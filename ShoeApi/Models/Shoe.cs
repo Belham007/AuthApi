2b4a15d035e23ef17584e68f3890201754d11496
Deepak kumar <deepakdare@outlook.com>
2023-09-16 19:03:09 +0530

    Add project files.

---
+++ ShoeApi/Models/Shoe.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoeApi.Models
+{
+    public class Shoe
+    {
+        public  int Id { get; set; }
+
+        public  string Name { get; set; }
+
+        public  string Brand { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
